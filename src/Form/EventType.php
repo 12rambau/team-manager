6b5d38e892ba74c736a42d46beab8a9a6c8f8c7a
12rambau <pierrick.rambaud@orange.fr>
2019-02-26 23:59:07 +0100

    ajout d'un datepicker dans les vues

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -8,12 +8,20 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\EventTag;
 
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('tag', EntityType::class, [
+                'class' => EventTag::class,
+                'choice_label'=>'name',
+                'expanded'=>true,
+                'multiple'=>false
+            ])
             ->add('name')
             ->add('start', DateTimeType::class,[
                 'date_widget'=>'single_text',
