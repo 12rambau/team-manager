40b45bdcea9403b791451475438d53af71adda3f
12rambau <pierrick.rambaud@orange.fr>
2019-03-17 15:02:43 +0100

    debug the button function

--- public/js/event/changeCheck.js
+++ public/js/event/changeCheck.js
@@ -1,6 +1,6 @@
 //function to change the radio button according to nicer link designed with bootstrap
 function changeCheckValue(item) {
-    var value = (item.id.slice == "1");
+    var value = (item.id.slice(-1) == "1");
     item.className = item.className.replace("btn-outline","btn");
     noItem = document.getElementById(item.id.slice(0,-1) + (!value | 0)  );
     if (noItem.className.search('outline') == -1) 
