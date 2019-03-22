da48c312d931d3d281681f88a64cc132fe37c710
12rambau <pierrick.rambaud@orange.fr>
2019-03-22 14:46:32 +0100

    use the build In fileType of Vich

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -6,14 +6,17 @@ use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ImageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('imageFile', FileType::class)
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                //'imagine_pattern' => '...',
+            ]);
         ;
     }
 
