bdc674997e9a16e8d266d5dc543e56a304c266be
12rambau <pierrick.rambaud@orange.fr>
2019-03-28 23:02:00 +0100

    remove the css and js from template page

---
+++ public/js/field/imageHeight.js
@@ -0,0 +1,4 @@
+//allows to adapt the size of the div arround the fild image to the size of the latter
+$(document).ready(function(){
+    $("#positionning").height($("#imageFile").height());
+});
---
+++ public/js/field/positionManager.js
@@ -0,0 +1,90 @@
+// setup an "add a tag" link
+var addPositionButton = $('<button type="button" class="btn btn-primary">Add a position</button>');
+var newLinkLi = $('<li class="list-group-item"></li>').append(addPositionButton);
+
+$(document).ready(function() {
+    // Get the ul that holds the collection of tags
+    var collectionHolder = $('#positions');
+
+    // add the "add a tag" anchor and li to the tags ul
+    collectionHolder.append(newLinkLi);
+
+    // count the current form inputs we have, use that as the new
+    // index when inserting a new item
+    collectionHolder.data('index', collectionHolder.find(':input').length);
+
+    addPositionButton.on('click', function(e) {
+        addPositionForm(collectionHolder, newLinkLi);
+    });
+});
+
+function addPositionForm(collectionHolder, newLinkLi) {
+    // Get the data-prototype explained earlier
+    var prototype = collectionHolder.data('prototype');
+
+    // get the new index
+    var index = collectionHolder.data('index');
+
+    var newForm = prototype;
+
+    // Replace '__name__' in the prototype's HTML
+    newForm = newForm.replace(/__name__/g, index);
+
+    // increase the index with one for the next item
+    collectionHolder.data('index', index + 1);
+
+    // Display the form in the page in an li, before the "Add a tag" link li
+    var newFormLi = $('<li class="list-group-item"></li>').append(newForm);
+    newLinkLi.before(newFormLi);
+
+    var card = $('<div/>',
+    {
+        id: newFormLi.children(":first").attr('id')+"_card",
+        "class": "card position-card",
+        css:{
+            "top": "0%",
+            "left": "0%",
+        }
+    });
+
+    var phantom = $('<img/>',
+    {
+        src: phantomUrl
+    });
+
+    //display the new phantom on the field
+    $("#positionning").append(card.append(phantom));
+
+
+}
+
+function removePosition(event){
+    //prevent the button from validating the form
+    event.preventDefault();
+    
+    //get the Id of the position
+    var name = $(event.target).prev().attr('id');
+    
+    //remove the phantom card
+    $("#"+name+"_card").remove();
+
+    //remove the position form
+    $(event.target).parent().remove();
+}
+
+
+$(document).on("change", 'input[type=range]', function(event){
+
+    //get the target HTML range input
+    var target = $(event.target);
+
+    //get the name of the position associated
+    var position = target.parent().parent().attr('id');
+    
+    //change vertical or horizontal value
+    if(target.attr('id') == position+"_horizontal")
+        $("#"+position+"_card").css('left', target.val()+"%");
+    else if(target.attr('id') == position+"_vertical")
+        $("#"+position+"_card").css('top', target.val()+"%");
+
+});
