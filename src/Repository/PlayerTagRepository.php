5c4a8eb52d663b7327832c573246fadef1f6ee99
12rambau <pierrick.rambaud@orange.fr>
2019-11-28 16:09:10 +0100

    add new entities

---
+++ src/Repository/PlayerTagRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\PlayerTag;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method PlayerTag|null find($id, $lockMode = null, $lockVersion = null)
+ * @method PlayerTag|null findOneBy(array $criteria, array $orderBy = null)
+ * @method PlayerTag[]    findAll()
+ * @method PlayerTag[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PlayerTagRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, PlayerTag::class);
+    }
+
+    // /**
+    //  * @return PlayerTag[] Returns an array of PlayerTag objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?PlayerTag
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
