4b25e38e5ad28d38c49b430a0b504596bb3a7614
12rambau <pierrick.rambaud@orange.fr>
2019-12-14 23:06:28 +0100

    update ckeditor confiuration

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,15 @@ Encore
     .addEntry('plannification', './assets/js/plannification.js')
     .addEntry('user', './assets/js/user.js')
 
+    //for Ckeditor installation
+    .copyFiles([
+        {from: './node_modules/ckeditor/', to: 'ckeditor/[path][name].[ext]', pattern: /\.(js|css)$/, includeSubdirectories: false},
+        {from: './node_modules/ckeditor/adapters', to: 'ckeditor/adapters/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/lang', to: 'ckeditor/lang/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/plugins', to: 'ckeditor/plugins/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/skins', to: 'ckeditor/skins/[path][name].[ext]'}
+    ])
+
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
